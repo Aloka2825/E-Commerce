15f6b944dd64e75af193af0c2d46ba1986dd6835
Aloka Moharana <alokmoharana23@yahoo.com>
2024-06-19 16:58:20 +0530

    URL Added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': 'http://localhost:8000'
+      '/api/v1': 'https://e-commerce-xq9x.onrender.com'
       }
   },
   plugins: [react()],
